2822f10adb71dc5838bfa032e2af384ac37dee8a
zesettler <gromovgrin@gmail.com>
2023-06-01 20:10:30 +0300

    first commit

---
+++ components/SidebarItem.tsx
@@ -0,0 +1,23 @@
+import { IconType } from "react-icons";
+
+interface SidebarItemProps {
+    icon: IconType;
+    label: string;
+    active?: boolean;
+    href: string;
+}
+
+const SidebarItem: React.FC<SidebarItemProps> = ({
+    icon,
+    label,
+    active,
+    href
+}) => {
+  return (
+    <div>
+        
+    </div>
+  );
+}
+
+export default SidebarItem;
