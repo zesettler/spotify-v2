5751608925ea985d7b4d368fdd6629496842b6c2
zesettler <gromovgrin@gmail.com>
2023-06-03 19:58:09 +0300

    Preparing to create login component

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { twJoin } from "tailwind-merge";
 import { RxCaretLeft,RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -41,11 +42,19 @@ const Header: React.FC<HeaderProps> = ({
                 <div className="flex justify-between items-center gap-x-4 ">
                     <>
                         <div>
-                            <Button />
+                            <Button onClick={() => {}} className="bg-transparent text-neutral-300 font-medium">
+                                Sign up
+                            </Button>
+                        </div>
+                        <div>
+                            <Button onClick={() => {}} className="bg-white px-6 py-2">
+                                Log in
+                            </Button>
                         </div>
                     </>
                 </div>
             </div>
+            {children}
         </div>
     );
 }
