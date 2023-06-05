cf0090004743d468c8d4429a52b49302eb0fc9e7
zesettler <gromovgrin@gmail.com>
2023-06-05 19:26:22 +0300

    upload modal

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import AuthModal from "@/components/AuthModal";
+import UploadModal from "@/components/UploadModal";
+
 import { useEffect, useState } from "react";
 
 const ModalProvider = () => {
@@ -17,6 +19,7 @@ const ModalProvider = () => {
     return (
         <>
             <AuthModal />
+            <UploadModal />
         </>
     );
 }
