11d7f195916679c486ba51325a7d3315fff7d61d
zesettler <gromovgrin@gmail.com>
2023-06-13 21:10:44 +0300

    stripe sec upd

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -32,4 +32,48 @@ export async function POST(
         console.log('Error message: ' + error.message);
         return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 })
     }
-}
+
+    if (relevantEvents.has(event.type)) {
+        try {
+            switch (event.type) {
+                case 'product.created':
+                case 'product.updated':
+                    await upsertProductRecord(event.data.object as Stripe.Product);
+                    break;
+                case 'price.created':
+                case 'price updated':
+                    await upsertPriceRecord(event.data.object as Stripe.Price);
+                    break;
+                case 'customer.subscription.created':
+                case 'customer.subscription.updated':
+                case 'customer.subscription.deleted':
+                    const subscripton = event.data.object as Stripe.Subscription;
+                    await manageSubscriptionStatusChange(
+                        subscripton.id,
+                        subscripton.customer as string,
+                        event.type === 'customer.subscription.created'
+                    );
+                    break;
+                case 'checkout.session.completed':
+                    const checkoutSession = event.data.object as Stripe.Checkout.Session;
+
+                    if (checkoutSession.mode === 'subscription') {
+                        const subscriptionId = checkoutSession.subscription;
+                        await manageSubscriptionStatusChange(
+                            subscriptionId as string,
+                            checkoutSession.customer as string,
+                            true
+                        );
+                    }
+                    break;
+                default:
+                    throw new Error('Unhandled relevant event!');
+            }
+        } catch (error) {
+            console.log(error);
+            return new NextResponse('Webhook error', { status: 400 });
+        }
+    }
+
+    return NextResponse.json({ recived: true }, { status: 200 });
+};
