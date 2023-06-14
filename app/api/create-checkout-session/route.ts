905ccd0e3bd073f89b392a658d6b0b84bdc56a7a
zesettler <gromovgrin@gmail.com>
2023-06-14 20:51:55 +0300

    upd: sub modal

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -1,51 +1,51 @@
-import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
-import { headers, cookies } from "next/headers";
-import { NextResponse } from "next/server";
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from "next/headers";
+import { NextResponse } from 'next/server';
 
-import { stripe } from "@/libs/stripe";
-import { getUrl } from "@/libs/helpers";
-import { createOrRetriveCutomer } from "@/libs/supabaseAdmin";
+import { stripe } from '@/libs/stripe';
+import { getURL } from '@/libs/helpers';
+import { createOrRetrieveCustomer } from '@/libs/supabaseAdmin';
 
 export async function POST(
-    request: Request
+  request: Request
 ) {
-    const { price, quantity = 1, metadata = {} } = await request.json();
-    
-    try {
-        const supabase = createRouteHandlerClient({
-            cookies
-        });
+  const { price, quantity = 1, metadata = {} } = await request.json();
 
-        const { data: { user } } = await supabase.auth.getUser();
+  try {
+    const supabase = createRouteHandlerClient({ 
+      cookies
+      });      const {
+      data: { user }
+    } = await supabase.auth.getUser();
 
-        const customer = await createOrRetriveCutomer({
-            uuid: user?.id || '',
-            email: user?.id || ''
-        });
+    const customer = await createOrRetrieveCustomer({
+      uuid: user?.id || '',
+      email: user?.email || ''
+    });
 
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types: ["card"],
-            billing_address_collection: 'required',
-            customer,
-            line_items: [
-                {
-                    price: price.id,
-                    quantity
-                }
-            ],
-            mode: 'subscription',
-            allow_promotion_codes: true,
-            subscription_data: {
-                trial_from_plan: true,
-                metadata
-            },
-            success_url: `${getUrl()}/account`,
-            cancel_url: `${getUrl()}`
-        });
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      billing_address_collection: 'required',
+      customer,
+      line_items: [
+        {
+          price: price.id,
+          quantity
+        }
+      ],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      subscription_data: {
+        trial_from_plan: true,
+        metadata
+      },
+      success_url: `${getURL()}/account`,
+      cancel_url: `${getURL()}/`
+    });
 
-        return NextResponse.json({ sessionId: session.id });
-    } catch (error: any) {
-        console.log(error);
-        return new NextResponse('Internal Error', { status: 500 })
-    }
+    return NextResponse.json({ sessionId: session.id });
+  } catch (err: any) {
+    console.log(err);
+    return new NextResponse('Internal Error', { status: 500 });
+  }
 }
--- components/SubscribeModal.tsx
+++ components/SubscribeModal.tsx
@@ -1,67 +1,124 @@
-'use client';
+"use client";
 
-import { Price, ProductWithPrice } from "@/types";
-import Modal from "./Modal";
-import Button from "./Button";
+import React, { useState } from 'react';
+import { toast } from 'react-hot-toast';
+
+import useSubscribeModal from '@/hooks/useSubscribeModal';
+import { useUser } from '@/hooks/useUser';
+import { postData } from '@/libs/helpers';
+import { getStripe } from '@/libs/stripeClient';
+import { Price, ProductWithPrice } from '@/types';
+
+import Modal from './Modal';
+import Button from './Button';
 
 interface SubscribeModalProps {
-    products: ProductWithPrice[];
+  products: ProductWithPrice[];
 }
 
 const formatPrice = (price: Price) => {
-    const priceString = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: price.currency,
-        minimumFractionDigits: 0
-    }).format((price?.unit_amount || 0) / 100);
+  const priceString = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: price.currency,
+    minimumFractionDigits: 0
+  }).format((price?.unit_amount || 0) / 100);
 
-    return priceString;
-}
+  return priceString;
+};
+
 
 const SubscribeModal: React.FC<SubscribeModalProps> = ({
-    products
+  products
 }) => {
-    let content = (
-        <div className="text-center">
-            No products avaliable.
-        </div>
-    );
+  const subscribeModal = useSubscribeModal();
+  const { user, isLoading, subscription } = useUser();
 
-    if (products.length) {
-        content = (
-            <div>
-                {products.map((product) => {
-                    if (!product.prices?.length) {
-                        return (
-                            <div key={product.id}>
-                                No prices avaliable
-                            </div>
-                        );
-                    }
+  const [priceIdLoading, setPriceIdLoading] = useState<string>();
 
-                    return product.prices.map((price) => (
-                        <Button 
-                        key={price.id} 
-                        onClick={() => handleCheckout(price)}
-                        disabled={isLoading || price.id === priceIdLoading}
-                        className="mb-4">
-                            {`Subscribe for ${formatPrice(price)} a ${price.interval}`}
-                        </Button>
-                    ))
-                })}
-            </div>
-        )
+  const onChange = (open: boolean) => {
+    if (!open) {
+      subscribeModal.onClose();
+    }
+  }
+
+  const handleCheckout = async (price: Price) => {
+    setPriceIdLoading(price.id);
+    if (!user) {
+      setPriceIdLoading(undefined);
+      return toast.error('Must be logged in');
     }
 
-    return (
-        <Modal 
-        title="Only for premium users" 
-        description="Listen to music with Spotify Premium" 
-        isOpen 
-        onChange={() => {}}>
-            {content}
-        </Modal>
-    );
+    if (subscription) {
+      setPriceIdLoading(undefined);
+      return toast('Already subscribed');
+    }
+
+    try {
+      const { sessionId } = await postData({
+        url: '/api/create-checkout-session',
+        data: { price }
+      });
+
+      const stripe = await getStripe();
+      stripe?.redirectToCheckout({ sessionId });
+    } catch (error) {
+      return toast.error((error as Error)?.message);
+    } finally {
+      setPriceIdLoading(undefined);
+    }
+  };
+
+  let content = (
+    <div className="text-center">
+      No products available.
+    </div>
+  )
+
+  if (products.length) {
+    content = (
+      <div>
+        {products.map((product) => {
+          if (!product.prices?.length) {
+            return (
+              <div key={product.id}>
+                No prices available
+              </div>
+            );
+          }
+
+          return product.prices.map((price) => (
+            <Button 
+              key={price.id} 
+              onClick={() => handleCheckout(price)}
+              disabled={isLoading || price.id === priceIdLoading}
+              className="mb-4"
+            >
+              {`Subscribe for ${formatPrice(price)} a ${price.interval}`}
+            </Button>
+          ))
+        })}
+      </div>
+    )
+  }
+
+  if (subscription) {
+    content = (
+      <div className="text-center">
+        Already subscribed.
+      </div>
+    )
+  }
+
+  return (
+    <Modal
+      title="Only for premium users"
+      description="Listen to music with Spotify Premium"
+      isOpen
+      onChange={onChange}
+    >
+      {content}
+    </Modal>
+  );
 }
 
 export default SubscribeModal;
